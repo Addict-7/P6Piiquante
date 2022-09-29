6b88449511a73bc366ad5137fa09b9979d4fa0a4
ELRLHZ <gicss27@hotmail.com>
2022-09-29 16:19:41 +0200

    Auth401

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 const express = require('express');
+require("dotenv").config();
 const mongoose = require('mongoose');
 const path = require('path');
+const helmet = require('helmet');
+const cors = require('cors');
+
+
 
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
-mongoose.connect('mongodb://Addict_95:jesuisunelicorne0@ac-nq9oc5b-shard-00-00.nkcjmkr.mongodb.net:27017,ac-nq9oc5b-shard-00-01.nkcjmkr.mongodb.net:27017,ac-nq9oc5b-shard-00-02.nkcjmkr.mongodb.net:27017/?ssl=true&replicaSet=atlas-g7cw20-shard-0&authSource=admin&retryWrites=true&w=majority',
+mongoose.connect(`mongodb://${process.env.USERID}:${process.env.PASSWORD}@ac-nq9oc5b-shard-00-00.nkcjmkr.mongodb.net:27017,
+                  ac-nq9oc5b-shard-00-01.nkcjmkr.mongodb.net:27017,ac-nq9oc5b-shard-00-02.nkcjmkr.
+                  mongodb.net:27017/?ssl=true&replicaSet=atlas-g7cw20-shard-0&authSource=admin&
+                  retryWrites=true&w=majority`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
+
 app.use(express.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 app.use(express.json());
+
+app.use(helmet());
+app.use(cors());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
--- middleware/auth.js
+++ middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(' ')[1];
-        const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+        const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN_KEY);
         const userId = decodedToken.userId;
         req.auth = {
             userId: userId
@@ -11,7 +11,9 @@ module.exports = (req, res, next) => {
         if (userId) {
             next();
         }
-    } catch(error) {
-        res.status(401).json({ error });
+    } catch {
+        return(error) => {
+            res.status(401).json({ error });
+        };
     } 
 };
